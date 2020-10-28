000d78ac6116a02f69b14794070a81f0b0e09350
Eric Endsley <eric.endsley4@gmail.com>
2020-10-28 16:25:38 -0700

    move the first letter of an input starting with a consonant to the end of the string, then add 'ay'
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,28 @@ $(document).ready(function(){
   $("#pigLatin").submit(function (event) {
     event.preventDefault();
     let pigLatinInput = $("#pigLatinInput").val();
-    console.log(pigLatinInput);
+
+    function pigLatin(pigLatinInput) {
+      const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', 'z'];
+      const vowels = ["a", "e", "i", "o", "u"];
+      if (vowels.includes(pigLatinInput[0])) {
+        pigLatinInput = pigLatinInput + "way";
+        // console.log(pigLatinInput);
+        return pigLatinInput;
+      } else if (consonants.includes(pigLatinInput[0])) {
+        // Method #1 using substrings
+        pigLatinInput = pigLatinInput.substr(1) + pigLatinInput.substr(0,1) + "ay"
+      
+        // Method #2 using RegExp
+        // let regExp = new RegExp(pigLatinInput[0]);
+        // let consonantGone = pigLatinInput.replace(regExp, "");
+        // console.log(consonantRemoved);
+        console.log(pigLatinInput);
+        return pigLatinInput;
+        
+      }
+      
+    };
+   pigLatin(pigLatinInput);
   });
 });
